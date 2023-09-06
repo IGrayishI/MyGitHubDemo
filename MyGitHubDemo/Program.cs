664f3f1c25c16655aea8d936d02e658f46dbf699
Benjo <Benjo@NANOVII>
2023-09-06 14:17:56 +0200

    Add project files.

---
+++ MyGitHubDemo/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MyGitHubDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, I totally do not get how I upload things to GitHub.");
+        }
+    }
+}
